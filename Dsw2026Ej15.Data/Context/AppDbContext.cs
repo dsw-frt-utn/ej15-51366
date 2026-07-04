0cdb8dd455e0b5f1e378f5683d03d0eb63515f93
Samir Morhill <Samir.Morhill@alu.frt.utn.edu.ar>
2026-07-03 21:36:31 -0300

    Configuracion de context realizado

--- Dsw2026Ej15.Data/Context/AppDbContext.cs
+++ Dsw2026Ej15.Data/Context/AppDbContext.cs
@@ -15,6 +15,25 @@ namespace Dsw2026Ej15.Data.Context
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Doctor>(eb =>
+            {
+                eb.HasKey(d => d.Id);
+                eb.Property(d => d.Name).IsRequired().HasMaxLength(100);
+                eb.Property(d => d.LicenseNumber).IsRequired().HasMaxLength(50);
+                eb.Property(d => d.IsActive).IsRequired();
+                eb.HasOne(d => d.Speciality)
+                  .WithMany()
+                  .HasForeignKey(d => d.SpecialityId)
+                  .OnDelete(DeleteBehavior.Restrict);
+
+            });
+
+            modelBuilder.Entity<Speciality>(eb =>
+            {
+                eb.HasKey(s => s.Id);
+                eb.Property(s => s.Name).IsRequired().HasMaxLength(100);
+                eb.Property(s => s.Description).HasMaxLength(500);
+            });
         }
-    }
+}
 }
